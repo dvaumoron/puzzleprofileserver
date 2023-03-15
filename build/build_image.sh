4e9438f0559b09585fdd84a83af2569b30b5a939
dvaumoron <dvaumoron@gmail.com>
2023-03-15 17:28:44 +0100

    add build_image.sh

---
+++ build/build_image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+go install
+
+buildah from --name puzzleprofileserver-working-container scratch
+buildah copy puzzleprofileserver-working-container $HOME/go/bin/puzzleprofileserver /bin/puzzleprofileserver
+buildah config --env SERVICE_PORT=50051 puzzleprofileserver-working-container
+buildah config --port 50051 puzzleprofileserver-working-container
+buildah config --entrypoint '["/bin/puzzleprofileserver"]' puzzleprofileserver-working-container
+buildah commit puzzleprofileserver-working-container puzzleprofileserver
+buildah rm puzzleprofileserver-working-container
